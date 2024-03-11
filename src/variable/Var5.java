38af50c1e528c5c7db56787c81636378cab4a521
universtar2123 <green-olive@kakao.com>
2024-03-11 11:26:58 +0900

    java-variable
    자바 변수 학습

---
+++ src/variable/Var5.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Var5 {
+    public static void main(String[] args) {
+        //1. 변수 선언, 초기화 각각 따로
+        int a;
+        a = 1;
+        System.out.println(a);
+
+        //2. 변수 선언, 초기화를 한번에
+        int b = 2;
+        System.out.println(b);
+
+        //3. 여러 번수 선언과 초기화를 한번에
+        int c = 3, d = 4;
+
+    }
+}
